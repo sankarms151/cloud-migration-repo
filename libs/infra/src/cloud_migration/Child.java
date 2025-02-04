296b9f33c668c3a577e8b2d6e2ca7387b82b631d
sankarms151 <sankarms151@gmail.com>
2025-02-04 14:11:22 +0530

    msg

--- libs/infra/src/cloud_migration/Child.java
+++
@@ -1,8 +0,0 @@
-package cloud_migration;
-
-public class Child {
-    public static void printMessage() {
-        System.out.println("Hello World");
-        System.out.println("04.02.2025");
-    }
-}
