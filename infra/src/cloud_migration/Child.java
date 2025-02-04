b7a345a070be66a3ffac79cb386e4ff9bf2584d6
sankarms151 <sankarms151@gmail.com>
2025-02-04 12:50:24 +0530

    removed

--- infra/src/cloud_migration/Child.java
+++
@@ -1,7 +0,0 @@
-package cloud_migration;
-
-public class Child {
-    public static void printMessage() {
-        System.out.println("Hello World");
-    }
-}
--- infra/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module demo_cloud_migration {
-}
